8f6ac6830d4b7e2c50603319e3a74e38666d45ff
Ricko00 <muhammadricko.mrc@gmail.com>
2025-02-19 11:04:13 +0700

    menambahkan file while_loop

---
+++ while_loop.js
@@ -0,0 +1,9 @@
+//while loop adalah versi perulangan yang lebih sederhan dibandingkan for loop
+//while loop, hanya terdapat kondisi perulangan, tanpa ada init statement dan post statement
+
+//Exampel 
+let ulang = 1
+while (ulang <= 10) {
+    console.log(`ini adalah while loop ke${ulang}`)
+    ulang++;
+}
