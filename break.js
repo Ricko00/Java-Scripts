03c52acbdda2fdafdfec803df30763827a31853b
Ricko00 <muhammadricko.mrc@gmail.com>
2025-02-21 15:22:35 +0700

    penambahan break.js

---
+++ break.js
@@ -0,0 +1,15 @@
+/**
+ * break : untuk menghentikan perulangan  
+ * 
+ */
+
+//cotoh break 
+let cunter =1;
+while (true) {
+    console.log(`ulagn ke ${cunter}`)
+    cunter++;
+
+    if (cunter > 2) {
+        break
+    }
+}
